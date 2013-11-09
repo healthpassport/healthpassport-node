c71c0ed6d99f6ee456cfb537c2b0d46e2d1b7f7f
Nicola Greco <notsecurity@gmail.com>
2013-11-09 10:26:56 +0000

    adding error api route

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,10 @@
 var Routes = {};
 Routes.json = function(req, res) {
   res.json(res.locals.json);
-}
+};
+
+Routes.err = function(req, res) {
+  res.json(500, res.locals.json);
+};
+
 module.exports = exports = Routes;
