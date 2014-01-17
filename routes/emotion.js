0dd3cddbb7d4c8828df5f6c0d1e87917ab39f277
nicolagreco <notsecurity@gmail.com>
2014-01-17 17:02:49 +0000

    additional files added

---
+++ routes/emotion.js
@@ -0,0 +1,34 @@
+var Model = require('../models/user');
+var db = require('../classes/mysql');
+var Routes = {};
+var __ = require('underscore');
+var async = require('async');
+var bcrypt = require('bcrypt');
+
+
+Routes.create = function(req, res, next) {
+
+  var values = {
+    uid: req.user.uid,
+    emotion_type: req.body.emotion_type,
+    date: new Date(),
+    location: req.body.location,
+    description: req.body.description
+  };
+
+  db.query('INSERT INTO emotions SET ?;', values, function(err, rows){
+    console.log("BBBB", err, rows)
+    if (err) return res.json(500, {status:"Error creating the user"});
+    res.locals.json = {status: "OK"};
+    next();
+  });
+}
+
+Routes.query = function(req, res, next) {
+  db.query('SELECT * FROM emotions WHERE uid=(SELECT uid FROM users WHERE username=?);', req.params.username, function(err, result) {
+    res.locals.json = result;
+    return next(err);
+  })
+}
+
+module.exports = exports = Routes;
