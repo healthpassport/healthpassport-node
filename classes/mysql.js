ce988ce0cdf848c0b4668c806f59f3f8b01b04d9
nicolagreco <notsecurity@gmail.com>
2014-01-20 23:44:27 +0000

    handling reconnection to database

--- classes/mysql.js
+++ classes/mysql.js
@@ -1,10 +1,29 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST || 'localhost',
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '',
-  database: process.env.MYSQL_DATABASE || 'healthpass'
-});
-connection.connect();
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST || 'localhost',
+    user: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD || '',
+    database: process.env.MYSQL_DATABASE || 'healthpass'
+  });
+  connection.connect(function(err) {
+    if(err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = exports = connection;
