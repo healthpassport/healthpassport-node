311c3ca8265ef9fc057c2c5af8e08c6bbc7ec44e
ralucacocioban <zcabrco@live.ucl.ac.uk>
2014-03-18 11:38:17 +0000

    port can now be defined

--- models/index.js
+++ models/index.js
@@ -5,7 +5,10 @@ var fs        = require('fs')
   , sequelize = new Sequelize(
       process.env.MYSQL_DATABASE || 'healthpass',
       process.env.MYSQL_USER || 'root',
-      process.env.MYSQL_PASSWORD || '')
+      process.env.MYSQL_PASSWORD || '', {
+        host:process.env.MYSQL_HOST || 'localhost',
+        port:process.env.MYSQL_PORT || 3306
+      })
   , db        = {}
  
 fs
